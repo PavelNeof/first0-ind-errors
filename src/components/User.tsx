0845ad7160c2dfc576380f8b20004900319a9efb
PavelNeof <pavelneof@gmail.com>
2022-08-30 22:13:08 +0300

    find errors

---
+++ src/components/User.tsx
@@ -0,0 +1,12 @@
+import React, {FC, memo} from 'react';
+import {UserType} from 'types';
+
+export const User: FC<UserType> = memo(({id, name}) => {
+    debugger
+  return (
+    <div>
+      <div>Name: {name}</div>
+      <div>Id: {id}</div>
+    </div>
+  );
+});
